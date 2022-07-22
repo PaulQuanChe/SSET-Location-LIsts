e86942e3addd4478f9308ca4ea78563c654ecb91
Quan <s3893635>
2022-07-22 10:09:18 +0700

    Initial Commit

---
+++ SSET Location LIsts/SSET_Location_LIstsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSET_Location_LIstsApp.swift
+//  SSET Location LIsts
+//
+//  Created by QuanChe on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SSET_Location_LIstsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
